a2f266a7632d2f26dab9d824676ba7d634293b9b
l3kov9 <l3kov.9@gmail.com>
2017-06-12 04:51:57 +0300

    Add Calculator

---
+++ Calculator/models/Calculator.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Lekov on 11.6.2017 г..
+ */
+
+function Calculator(leftOperand, operator, rightOperand){
+    this.leftOperand=leftOperand;
+    this.operator=operator;
+    this.rightOperand=rightOperand;
+
+    this.calculateResult=function () {
+        let result=0;
+
+        switch (this.operator){
+            case "+":
+                result=this.leftOperand + this.rightOperand;
+                break;
+            case "-":
+                result=this.leftOperand - this.rightOperand;
+                break;
+            case "*":
+                result=this.leftOperand * this.rightOperand;
+                break;
+            case "/":
+                result=this.leftOperand / this.rightOperand;
+                break;
+        }
+
+        return result;
+    }
+}
+
+module.exports = Calculator;
