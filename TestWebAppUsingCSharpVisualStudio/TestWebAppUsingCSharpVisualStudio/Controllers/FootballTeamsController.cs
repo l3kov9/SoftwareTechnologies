6391fb0a8407992b2288a29ad21e09b8b2b19e8f
l3kov9 <l3kov.9@gmail.com>
2017-06-19 06:13:18 +0300

    Adding C# Blog

---
+++ TestWebAppUsingCSharpVisualStudio/TestWebAppUsingCSharpVisualStudio/Controllers/FootballTeamsController.cs
@@ -0,0 +1,20 @@
+﻿
+namespace TestWebAppUsingCSharpVisualStudio.Controllers
+{
+    using System.Web.Mvc;
+
+    public class FootballTeamsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestWebAppUsingCSharpVisualStudio/TestWebAppUsingCSharpVisualStudio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebAppUsingCSharpVisualStudio.Startup))]
+namespace TestWebAppUsingCSharpVisualStudio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
